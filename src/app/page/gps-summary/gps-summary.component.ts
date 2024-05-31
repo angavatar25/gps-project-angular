75cfc1ecb8ee4fe667246f8dcd98a7bd76bb53b1
Ananda Afryan Gemilang <anandalark007@gmail.com>
2024-05-31 14:38:23 +0700

    feat: add gps summary pagination

--- src/app/page/gps-summary/gps-summary.component.ts
+++ src/app/page/gps-summary/gps-summary.component.ts
@@ -10,5 +10,34 @@ import { GpsData } from '../../dummy/GPSData';
   styleUrl: './gps-summary.component.scss'
 })
 export class GpsSummaryComponent {
+  start = 0
+  end = 0
+  currentPage: number = 1
+  itemsPerPage: number = 5
   gpsDataCollection = GpsData;
+  totalPages: number[] = [];
+
+  constructor() {
+    this.totalPages = this.generateNumbers(Math.ceil(this.gpsDataCollection.length / this.itemsPerPage));
+    this.start = (this.currentPage - 1) * this.itemsPerPage;
+    this.end = this.start + this.itemsPerPage;
+    this.gpsDataCollection = GpsData.slice(this.start, this.end);
+  }
+  
+
+  displayPage(page: number) {
+    const start = (page - 1) * this.itemsPerPage;
+    const end = start + this.itemsPerPage;
+    this.gpsDataCollection = GpsData.slice(start, end);
+  }
+
+  generateNumbers(count: number): number[] {
+    return Array.from({length: count}, (_, i) => i + 1);
+  }
+
+  renderPaginationStyle(page: number) {
+    if (page === this.currentPage) return 'bg-blue-500 text-white';
+
+    return 'bg-transparent text-black';
+  }
 }
