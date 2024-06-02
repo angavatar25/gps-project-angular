51e99d173455439b09d4238a2abfb9fe9ffb8ef3
Ananda Afryan Gemilang <anandalark007@gmail.com>
2024-06-02 21:56:06 +0700

    feat: add chart on detail gps page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { BaseChartDirective } from 'ng2-charts';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, BaseChartDirective],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
